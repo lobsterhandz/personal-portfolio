f5477efa78cfce35218c4d04ed06253c158d18fd
lobsterhandz <79618439+lobsterhandz@users.noreply.github.com>
2025-03-14 16:10:02 -0400

    solar system13

--- solar_system.js
+++ solar_system.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.128.0/build/three.module.js';
+import * as THREE from './three.module.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
 
