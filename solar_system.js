c3c257046998c4518ad38ee650aa57a3008f7b36
lobsterhandz <79618439+lobsterhandz@users.noreply.github.com>
2025-03-14 15:46:59 -0400

    solar system111

--- solar_system.js
+++ solar_system.js
@@ -1,6 +1,6 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.min.js';
-import { OrbitControls } from './OrbitControls.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.140.2/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/examples/jsm/loaders/GLTFLoader.js';
 
 // --- Scene Setup ---
 const scene = new THREE.Scene();
