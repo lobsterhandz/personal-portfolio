2e3bafe61181e8337ebad1b506ee948e3e326046
lobsterhandz <79618439+lobsterhandz@users.noreply.github.com>
2025-03-14 12:22:29 -0400

    solar system1.1

--- galaxy.js
+++ galaxy.js
@@ -1,4 +1,5 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.128.0/build/three.module.js';
+// galaxy.js
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.min.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
 
